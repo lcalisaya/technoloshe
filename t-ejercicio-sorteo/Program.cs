bc13571acedf650e14afdb19af25d9dfc3348000
Lucia Calisaya <calisaya.lucia@gmail.com>
2017-10-19 20:25:33 -0300

    Ejercicio de sorteo hecho con una lista

--- t-ejercicio-sorteo/Program.cs
+++ t-ejercicio-sorteo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace t_ejercicio_sorteo
 {
@@ -6,6 +7,7 @@ namespace t_ejercicio_sorteo
     {
         static void Main(string[] args)
         {
+            /* ejercicio hecho con un array
             string[] participantes = new string[12];
             Console.WriteLine("Vamos a hacer un sorteo. Deben participar hasta 12 personas");
 
@@ -52,6 +54,55 @@ namespace t_ejercicio_sorteo
                 {
                     randomPerson = random.Next(0, 12);  
                 }
+
+                //Mostrar el participante que ganó el sorteo
+                Console.WriteLine("El ganador/a del sorteo es: {0}!", participantes[randomPerson]);
+
+            }
+        */
+
+            List<string> participantes = new List<string>();
+            Console.WriteLine("Vamos a hacer un sorteo con cualquier número de participantes");
+
+            //Llenar el array con los participantes
+            string entrada = "";
+            Console.WriteLine("Ingrese el nombre de 1 participante o 'listo' para salir:");
+            entrada = Console.ReadLine();
+
+            while (entrada != "listo")
+            {
+                if (entrada == "")
+                {
+                    Console.WriteLine("El nombre no puede estar vacío. Ingrese un nombre o 'listo' para salir:");
+                    entrada = Console.ReadLine();
+                }
+                else
+                {
+                    participantes.Add(entrada);
+                    Console.WriteLine("Ingrese el nombre de 1 participante o 'listo' para salir:");
+                    entrada = Console.ReadLine();
+
+                }
+
+            }
+
+            //Mostrar los participantes ingresados
+            Console.WriteLine("Los participantes son:");
+
+            if (participantes.Count == 0)
+            {
+                Console.WriteLine("No se ingresaron participantes");
+            }
+            else
+            {
+                foreach (string participante in participantes)
+                {
+                    Console.WriteLine(participante);
+                }
+
+                //Seleccionar aleatoriamente un participante
+                Random random = new Random();
+                int randomPerson = random.Next(0, (participantes.Count));
 
                 //Mostrar el participante que ganó el sorteo
                 Console.WriteLine("El ganador/a del sorteo es: {0}!", participantes[randomPerson]);
