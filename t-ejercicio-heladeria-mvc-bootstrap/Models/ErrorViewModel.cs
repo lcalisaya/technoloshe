5707d8d95293d031555b0076bc1f9820ab8e55c3
Lucia Calisaya <calisaya.lucia@gmail.com>
2017-11-23 11:18:08 -0300

    Se inicia el proyecto mvc aplicando los estilos de Bootstrap

---
+++ t-ejercicio-heladeria-mvc-bootstrap/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace t_ejercicio_heladeria_mvc_bootstrap.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
