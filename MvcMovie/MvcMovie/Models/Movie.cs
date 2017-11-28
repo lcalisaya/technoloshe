ff27dd90edfaf1215e07d2ba3241868a094b4753
Lucia Calisaya <calisaya.lucia@gmail.com>
2017-11-28 14:07:57 -0300

    Se agrega la clase (Modelo) Movie con sus propiedades

---
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+// En orden se ser capaz de referenciar a DbContext y DbSet
+using System.Data.Entity;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    // La clase MovieDBContext representa el contexto de la BD movie en Entity Framework, el cual 
+    // manejará la búsqueda, el almacenamiento y la actualización de las instancias de la clase Movie
+    // en una base de datos.
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
