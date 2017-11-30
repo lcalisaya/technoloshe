41d1b99d2578f441679ddaf477e1adc86b4095b1
Lucia Calisaya <calisaya.lucia@gmail.com>
2017-11-30 16:30:48 -0300

    Se agrega el componente DataAnnotations y la librería globalize.js

--- MvcMovie/MvcMovie/Models/Movie.cs
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-
+using System.ComponentModel.DataAnnotations;
 // En orden de ser capaz de referenciar a DbContext y DbSet
 using System.Data.Entity;
+
 
 namespace MvcMovie.Models
 {
@@ -9,6 +10,13 @@ namespace MvcMovie.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        // Es lo mismo [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
