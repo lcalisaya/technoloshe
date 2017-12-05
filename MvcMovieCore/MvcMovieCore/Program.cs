26f26dc3951b7ef93dee4ba74b4df0cc8f553b45
Lucia Calisaya <calisaya.lucia@gmail.com>
2017-12-05 15:45:10 -0300

    Creación de la clase SeedData para guardar películas en la tabla Movie

--- MvcMovieCore/MvcMovieCore/Program.cs
+++ MvcMovieCore/MvcMovieCore/Program.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovieCore.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace MvcMovieCore
@@ -14,7 +11,25 @@ namespace MvcMovieCore
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    // Requires using MvcMovie.Models;
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
